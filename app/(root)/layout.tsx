7833977f66060f01b64f4e82602e0ea68775d40e
Robert Duke <robduke123@gmail.com>
2024-10-01 20:15:44 +1300

    first commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,14 @@
+export const dynamic = 'force-dynamic'
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <main>
+        SIDEBAR
+        {children}
+    </main>
+  );
+}
